e3db8e4a68babfd497c1926fc038d2af69107dc5
Normape <39561231+Normape@users.noreply.github.com>
2019-12-16 13:49:01 -0500

    Agregando script para correr las apps
    estos escrib ejecutan servicios

---
+++ scripts/deploy-backend.sh
@@ -0,0 +1,10 @@
+mkdir -p $PWD/server/other
+ln -s $PWD/shared $PWD/server/other/
+
+cd server
+npm install -S ./other/shared
+npx --package now@9 --call "now -e NODE_ENV=production --token $NOW_TOKEN --npm deploy --public && now alias --token=$NOW_TOKEN"
+
+# restore things (in case someone runs this locally by mistake)
+rm -rf server/other/shared
+npm install -S ../shared
